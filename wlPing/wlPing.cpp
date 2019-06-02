7a486e1e8e0daccda59c4888a044ea7d6b918fb3
梁 王 <wangliang1997@outlook.com>
2019-06-02 17:15:20 +0800

    添加项目文件。

---
+++ wlPing/wlPing.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <winsock2.h>
+#include "pch.h"
+
+
+int main(void)
+{
+	CPing objPing;
+
+	const char* cszDestIP = "127.0.0.1";
+	char* szDestIP = new char[100];
+
+	
+	strcpy_s(szDestIP, strlen(szDestIP)+1,cszDestIP);
+
+
+	PingReply reply;
+
+	cout << "Pinging  with  bytes of data:" << szDestIP << DEF_PACKET_SIZE ;
+	while (TRUE)
+	{
+		objPing.Ping(szDestIP, &reply);
+		cout << "Reply from  bytes= time= ldms TTL="<< szDestIP<< reply.m_dwBytes<< reply.m_dwRoundTripTime<<reply.m_dwTTL;
+		Sleep(500);
+	}
+
+	return 0;
+}
